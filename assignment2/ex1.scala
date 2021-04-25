befe91551ac1c056b64c17f67c2096a6c93f9e32
shahzaib <shahzaibceo@gmail.com>
2021-04-25 05:36:24 +0500

    Ass 2 Ex 1

---
+++ assignment2/ex1.scala
@@ -0,0 +1,14 @@
+package assignment2
+
+object ex1 extends App {
+
+  var a1:Array[String] = new Array[String](3)
+  a1(0) = "S"; a1(1) = "H"; a1(2) = "A"
+
+  val a2 = Array("S","H","A")
+
+  val a3:Array[String] = Array("S","H","A")
+
+
+
+}
